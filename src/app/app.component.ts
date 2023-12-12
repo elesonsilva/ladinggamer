ada9a1fba7987df8dcdef48f3304efe7fe1433d5
Éleson Silva <elesonsilva9@gmail.com>
2023-12-12 15:13:37 -0300

    aprendendo angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopoComponent } from "./components/topo/topo.component";
+import { PagePrincipalComponent } from "./components/page-principal/page-principal.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, TopoComponent, PagePrincipalComponent]
 })
 export class AppComponent {
-  title = 'ladinggamer';
+  title = 'JoyGamer';
 }
---
+++ src/app/components/page-principal/page-principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-principal',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-principal.component.html',
+  styleUrl: './page-principal.component.css'
+})
+export class PagePrincipalComponent {
+
+}
